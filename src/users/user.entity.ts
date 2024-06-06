5ab49de9b6f438681555d551d17f8684cf8fd71c
DeylerAF <deylerafernandez@outlook.com>
2024-06-06 15:05:22 -0600

    Add endpoint to create a new user, profile, posts

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,20 +1,35 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Profile } from './profile.entity';
+import { Post } from '../posts/post.entity';
 
-@Entity({name: 'users'})
+@Entity({ name: 'users' })
 export class User {
-
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true})
+  @Column({ unique: true })
   username: string;
 
   @Column()
   password: string;
 
-  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
 
-  @Column({nullable: true})
+  @Column({ nullable: true })
   authStrategy: string;
+
+  @OneToOne(() => Profile)
+  @JoinColumn()
+  profile: Profile;
+
+  @OneToMany(() => Post, post => post.author)
+  posts: Post[];
 }
